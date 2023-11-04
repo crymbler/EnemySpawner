1ac16d558160ba6ffaf1d3aaa1f56dfdefdc0948
crymbler <rayushkin.nikita@yandex.ru>
2023-11-04 23:48:56 +0300

    project commit

---
+++ EnemySpawner/Assets/Scripts/Spawner.cs
@@ -0,0 +1,44 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    [SerializeField] private Transform _pointContainer;
+    [SerializeField] private Enemy _enemyPrefab;
+    [SerializeField] private float _spawnDelay;
+    [SerializeField] private float _timeToDestroy;
+
+    private Transform[] _VectorSpawnpoints;
+    private float _nextSpawnDelay;
+
+    private void Start()
+    {
+        _VectorSpawnpoints = new Transform[_pointContainer.childCount];
+
+        for (int i = 0; i < _pointContainer.childCount; i++)
+        {
+            _VectorSpawnpoints[i] = _pointContainer.GetChild(i);
+        }
+    }
+
+    private void Update()
+    {
+        if (Time.time > _nextSpawnDelay)
+        {
+            _nextSpawnDelay = Time.time + _spawnDelay;
+
+            SpawnEnemy();
+        }
+    }
+
+    private void SpawnEnemy()
+    {
+        int randomNumber = Random.Range(0, _VectorSpawnpoints.Length);
+
+        Vector2 objectPosition = _VectorSpawnpoints[randomNumber].position;
+        Enemy enemy = Instantiate(_enemyPrefab, objectPosition, Quaternion.identity);
+
+        Destroy(enemy.gameObject, _timeToDestroy);
+    }
+}
