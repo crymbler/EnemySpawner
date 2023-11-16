4b8651b337dad1c15d4c64647aea83524e3d2904
crymbler <59792139+crymbler@users.noreply.github.com>
2023-11-17 00:07:24 +0300

    Update EnemyMover.cs

--- EnemySpawner/Assets/Scripts/EnemyMover.cs
+++ EnemySpawner/Assets/Scripts/EnemyMover.cs
@@ -7,10 +7,12 @@ public class EnemyMover : MonoBehaviour
     [SerializeField] private int _speed;
 
     private Vector2 _direction;
-
+    private int _xDirection = 0;
+    private int _yDirection = 1;
+    
     private void Awake()
     {
-        _direction = new Vector2(0, 1);
+        _direction = new Vector2(_xDirection, _yDirection);
     }
 
     private void Update()
